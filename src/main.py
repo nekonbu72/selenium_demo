529b05506de568fb3c091d2429d0d0f5c3a8034a
s150209 <s150209@ad.sei.co.jp>
2019-12-14 22:20:33 +0900

    .

--- src/main.py
+++ src/main.py
@@ -19,52 +19,50 @@ for name in ["mimeTypes.rdf", "handlers.json"]:
     if p.is_file():
         p.unlink()
 
-driver = webdriver.Firefox(
+with webdriver.Firefox(
     firefox_profile=fp,
     firefox_binary=Config.FIREFOX,
     executable_path=Config.GECKODRIVER,
     log_path=Config.LOG
-)
+) as driver:
 
-driver.implicitly_wait(2)
+    driver.implicitly_wait(2)
 
-driver.get(Secret.URL)
-driver.find_element_by_name("sei_login").send_keys(Secret.SSO_ID)
-driver.find_element_by_name("sei_passwd").send_keys(Secret.SSO_PASSWORD)
-driver.find_element_by_name("login").submit()
+    driver.get(Secret.URL)
+    driver.find_element_by_name("sei_login").send_keys(Secret.SSO_ID)
+    driver.find_element_by_name("sei_passwd").send_keys(Secret.SSO_PASSWORD)
+    driver.find_element_by_name("login").submit()
 
-sleep(2)
+    sleep(2)
 
-driver.switch_to.frame("fr_menu")
-driver.find_element_by_css_selector(
-    "body > table:nth-child(8) > tbody > tr:nth-child(2) > td > a").click()
+    driver.switch_to.frame("fr_menu")
+    driver.find_element_by_css_selector(
+        "body > table:nth-child(8) > tbody > tr:nth-child(2) > td > a").click()
 
-driver.switch_to.parent_frame()
-driver.switch_to.frame("fr_main")
+    driver.switch_to.parent_frame()
+    driver.switch_to.frame("fr_main")
 
-sleep(1)
+    sleep(1)
 
-driver.find_element_by_name("kensyu_nm").send_keys(Secret.SEARCH)
-driver.find_element_by_name("btn_submit").submit()
+    driver.find_element_by_name("kensyu_nm").send_keys(Secret.SEARCH)
+    driver.find_element_by_name("btn_submit").submit()
 
-sleep(2)
+    sleep(2)
 
-driver.find_element_by_id("pms_SeiResult2_link_vd_semdate_fne01_0").click()
+    driver.find_element_by_id("pms_SeiResult2_link_vd_semdate_fne01_0").click()
 
-driver.find_element_by_css_selector(
-    "#pms_dtl2_pms_SeiResult2_link_vd_detailview_fne01_0").click()
+    driver.find_element_by_css_selector(
+        "#pms_dtl2_pms_SeiResult2_link_vd_detailview_fne01_0").click()
 
-default_window = driver.current_window_handle
-for handle in driver.window_handles:
-    driver.switch_to.window(handle)
-    if driver.title == Secret.WINDOW:
-        break
+    default_window = driver.current_window_handle
+    for handle in driver.window_handles:
+        driver.switch_to.window(handle)
+        if driver.title == Secret.WINDOW:
+            break
 
-sleep(2)
+    sleep(2)
 
-driver.find_element_by_link_text(Secret.LINK).click()
+    driver.find_element_by_link_text(Secret.LINK).click()
 
-driver.switch_to.window(default_window)
-sleep(60)
-
-driver.quit()
+    driver.switch_to.window(default_window)
+    sleep(10)
